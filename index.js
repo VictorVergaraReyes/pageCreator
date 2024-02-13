8e3aa03d8eea38d4b3d63241fea97dd76fe0f4a8
vicvergara <vvergara@ahorra.io>
2024-02-12 18:15:47 -0600

    eneable cors for all routes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const fs = require('fs');
 const app = express()
-
+const cors = require('cors')
+const path = require('path');
 // app.get('/',(req,res) => {
 //     res.send('hello world')
 // })
@@ -10,6 +11,8 @@ const app = express()
 //     res.status(404).send('This route does not exists')
 // })
 
+
+app.use(cors());
 app.get('/templates', (req,res) => {
     res.send('This is your list of templates')
 })
@@ -19,6 +22,19 @@ app.post('/templates/view',(req,res) => {
         root:__dirname
     })
 })
+
+app.get('/templates/view/show', (req, res) => {
+    // Lee el archivo HTML y conviértelo en una cadena
+    let htmlContent = fs.readFileSync(path.join(__dirname, './templates/template1.html'), 'utf8');
+     
+    // Crea un objeto JSON con la cadena HTML como valor de una propiedad
+    let responseJson = {
+      htmlContent: htmlContent
+    };
+     
+    // Envia el objeto JSON como respuesta
+    res.json(responseJson);
+  });
 
 app.listen(3000)
 console.log(`Server on port 3000`);
